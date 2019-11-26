d7f9335147a2999d8e47028a570c67fa9a97006b
Jerwin Fernandez <jerwinfernandez04@gmail.com>
2019-11-26 22:34:39 +0800

    Adding roll dice and adding dice counter feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,76 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 6;
+  int die = 0;
+
+  void changeDie() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(5) + 1;
+    die = leftDiceNumber + rightDiceNumber;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Dice Count:',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0.0, 25.0, 0.0, 25.0),
+                child: Text(
+                  die == 0 ? 'Roll Dice' : '$die',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDie();
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDie();
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
